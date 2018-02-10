b997c49621e5b6581e570f154ec22038921b1d02
Sylvain <slevy.work@gmail.com>
2018-02-10 17:04:33 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+#TODO
+Library for RN development
---
+++ components/buttons/RoundedButton.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {TouchableOpacity,Text,View} from 'react-native'
+
+export default class RoundedButton extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  componentWillMount() {}
+  render() {
+    return (<TouchableOpacity>
+      <Text>Test</Text>
+    </TouchableOpacity>)
+  }
+}
